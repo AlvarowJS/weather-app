150d5463a4602750899a3dc45b1c4f9310d44796
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2022-06-06 21:43:14 -0500

    project finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,60 @@
 import { useEffect, useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import axios from 'axios'
+import CardWeather from './components/CardWeather'
+import Loader from './components/Loader'
+
 
 function App() {
-  const [obj, setObj] = useState()
-  // const [grade, setGrade] = useState(true)
-  const [weather, setWeather] = useState({})
+  const [latLon, setLatLon] = useState({})
+  const [weather, setWeather] = useState()
+
+  const [isLoading, setIsLoading] = useState(true)
+  
   let lon, lat
 
 
-  const getLatLong = () => {
+  useEffect(() => {
     const success = pos => {
       lon = pos.coords.longitude
       lat = pos.coords.latitude
-      console.log(lon, lat)
-      setObj({lat, lon})
+      setLatLon({lat, lon})
     }
     navigator.geolocation.getCurrentPosition(success)
-  }
-
-  // const switchGrade = () => setGrade(!grade)
-
-  const API_KEY = 'baff4e2e54b2fba992d989a46b2657ba'
+  }, [])
+  
 
 
 
   useEffect(() => {
-    if(obj !== undefined ){
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${obj?.lat}&lon=${obj?.lon}&appid=${API_KEY}`
+    if(latLon.lat !== undefined ){
+      const API_KEY = 'baff4e2e54b2fba992d989a46b2657ba'
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latLon.lat}&lon=${latLon.lon}&appid=${API_KEY}`
       axios.get(url)
-        .then(res => setWeather(res.data))
+        .then(res => {
+          setWeather(res.data)
+          setIsLoading(false)
+        })
         .catch(err => console.log(err))
+        // .finally(()=> setIsLoading(false))
     }
     
-  }, [obj])
+  }, [latLon])
 
-  const kelvin = weather.main.temp
-  const celsio =  (kelvin - 273.15).toFixed(2)
-  const fahrenheit = (celsio * (9/5) + 32).toFixed(2)
-  console.log('Fuera castillo',weather)
+ 
+
+
 
   return (
-    <div className="App">
-      <button onClick={getLatLong}>Get location</button>
-      <ul>
-        <li>{`${weather.name}, ${weather.sys.country}`}</li>
-        <li>{`${weather.weather[0].description}`}</li>
-        <li>{`${weather.wind.speed}`}</li>
-        <li>{`${weather.clouds.all}`}</li>
-        <li>{`${weather.main.pressure}`}</li>
-        
-        <li>{`${celsio} - ${fahrenheit} `}</li>
-      </ul>
-      <button onClick={switchGrade}> cambia</button>
+    <div className="inicio">
+    {
+      isLoading ?
+        <Loader />
+      :        
+        <CardWeather weather={weather} />
+    }              
+    
+      
     </div>
   )
 }
---
+++ src/components/CardWeather.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const CardWeather = ({ weather }) => {
+    const [grade, setGrade] = useState(false)
+
+    const switchGrade = () => {
+        setGrade(!grade)
+    }
+
+    const kelvin = weather?.main.temp
+    const celsio = (kelvin - 273.15).toFixed(2)
+    const fahrenheit = (celsio * (9 / 5) + 32).toFixed(2)
+    const getWheater = weather?.weather[0].icon
+
+
+    return (
+        <div className='app'>            
+                <p className='app__country'>{`${weather?.name}, ${weather?.sys.country}`} </p>                           
+            <img className='app__img' src={weather && `http://openweathermap.org/img/wn/${getWheater}@4x.png`} alt="icon wheater condition" />
+            <div className='app__core'>
+                <p className='app__core-grade'>{grade ? celsio + ' °C' : fahrenheit + ' °F'}</p>
+                <button className='app__core-button' onClick={switchGrade}> Cambiar a {grade ? '°F' : '°C'}</button>
+                <p className='app__core-description'>{`${weather?.weather[0].description}`}</p>
+
+            </div>
+
+            <div className='app__secondary'>
+                <div className='app__secondary-date'>
+                    <p>Wind</p>
+                    <i class='bx bx-wind'></i>
+                    <p>{`${weather?.wind.speed} km/h`}</p>
+                </div>
+
+                <div className='app__secondary-date'>
+
+                    <p>Clouds</p>
+                    <i class='bx bx-cloud'></i>
+                    <p>{`${weather?.clouds.all} %`}</p>
+                </div>
+                <div className='app__secondary-date'>
+                    <p>Humidity</p>
+                    <i class='bx bx-droplet' ></i>
+                    <p>{`${weather?.main.humidity} %`}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardWeather
